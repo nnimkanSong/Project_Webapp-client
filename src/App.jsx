11558c867a7d357f5cdb74508b5d0800291ef6ed
lamzacknon <66200059@kmitl.ac.th>
2025-09-11 12:24:40 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ const App = () => {
         <Route
           path="/Booking"
           element={
-            <ProtectedRoute>
+            <PublicRoute>
               <Booking />
-            </ProtectedRoute>
+            </PublicRoute>
           }
         />
 
