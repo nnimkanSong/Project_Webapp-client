d411500a2b162640eb9033275dcafd948010540e
nathakon <66200345@kmitl.ac.th>
2025-10-15 16:18:39 +0700

    set path 1.5

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-// client/vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: "./",                     // สำคัญมาก! ให้ asset path ถูกตอน deploy
-  server: {
-    port: 5174,
-    proxy: {
-      // ใช้เฉพาะตอน dev เท่านั้น
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-});
+  base: './'
+})
