91e2c1ae21f782d3e6389e66559adf524ce0d978
nathakon <66200345@kmitl.ac.th>
2025-11-01 14:26:08 +0700

    set vercel.json change rewrite3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "/",                     // สำคัญมาก! ให้ asset path ถูกตอน deploy
+  server: {
+    port: 5174,
+    proxy: {
+      // ใช้เฉพาะตอน dev เท่านั้น
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
