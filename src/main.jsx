81b57533d117b44619b52c8d91f076deed24423a
nathakon <66200345@kmitl.ac.th>
2025-11-01 17:05:12 +0700

    set nivo function size8

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css";
+import "@fontsource/ledger/400.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
